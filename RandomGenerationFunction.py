d17fba46a34e83356ee0c3d4720131c8c8417e28
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-19 20:37:55 +0545

    Standarized Random Numbers Generation in NumPy

--- RandomGenerationFunction.py
+++ RandomGenerationFunction.py
@@ -4,3 +4,8 @@ import numpy as np
 normalized_random = np.random.rand(5) # 5 random numbers between 0 and 1
 print("Normalized Random Numbers:")
 print(normalized_random)
+
+# Standarized random numbers
+standardized_random = np.random.randn(5) # 5 random numbers from a standard normal distribution
+print("\nStandardized Random Numbers:")
+print(standardized_random)
