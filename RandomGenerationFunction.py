d4531df2c7ebdbea64dfa499476f52c28587f02b
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-19 20:37:03 +0545

    Normalized Random Numbers Generation in NumPy

---
+++ RandomGenerationFunction.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+# Normalized random numbers
+normalized_random = np.random.rand(5) # 5 random numbers between 0 and 1
+print("Normalized Random Numbers:")
+print(normalized_random)
