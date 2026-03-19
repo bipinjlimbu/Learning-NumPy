37368713317bb8fb67ecfd9afed294e04949edef
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-19 20:39:55 +0545

    Random Integers Generation in NumPy

--- RandomGenerationFunction.py
+++ RandomGenerationFunction.py
@@ -1,11 +1,16 @@
 import numpy as np
 
 # Normalized random numbers
-normalized_random = np.random.rand(5) # 5 random numbers between 0 and 1
+normalized_random = np.random.rand(5) # 5 random numbers between 0 and 1 (size)
 print("Normalized Random Numbers:")
 print(normalized_random)
 
 # Standarized random numbers
-standardized_random = np.random.randn(5) # 5 random numbers from a standard normal distribution
+standardized_random = np.random.randn(5) # 5 random numbers from a standard normal distribution (size)
 print("\nStandardized Random Numbers:")
 print(standardized_random)
+
+# Random integers
+random_integers = np.random.randint(0, 10, 5) # 5 random integers between 0 and 9 (start, stop, size)
+print("\nRandom Integers:")
+print(random_integers)
