e619049f4e6a93de87d8169409ecb90af54de891
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-19 20:29:13 +0545

    Zeros and Ones Function in NumPy

--- ArrayGenerationFunction.py
+++ ArrayGenerationFunction.py
@@ -3,3 +3,11 @@ import numpy as np
 # Range function
 arr = np.arange(0, 10, 2) # start, stop, step
 print(arr)
+
+# Zeros and Ones
+zeros = np.zeros((3, 4)) # shape (rows, columns)
+print("\nZeros:")
+print(zeros)
+ones = np.ones((2, 5))
+print("\nOnes:")
+print(ones)
