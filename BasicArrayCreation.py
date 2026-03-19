8b19f014a5533c4d6d61595e7d5da8c17ed66220
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-19 20:25:20 +0545

    Basic Array Creation in NumPy

---
+++ BasicArrayCreation.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+print(arr)
+
+l = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+arr2 = np.array(l)
+print("\nList:")
+print(l)
+print("\nArray:")
+print(arr2)
