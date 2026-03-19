985582365df5e5dd80eaaaf18a1361b9d4048bcc
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-19 20:49:26 +0545

    Array Attributes in NumPy

---
+++ ArrayAttributes.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print("Array:")
+print(arr)
+
+# Attributes
+print("\nShape:", arr.shape) # shape of the array (rows, columns)
+print("Size:", arr.size) # total number of elements in the array
+print("Data Type:", arr.dtype) # data type of the elements in the array
+print("Number of Dimensions:", arr.ndim) # number of dimensions (axes) in the array
