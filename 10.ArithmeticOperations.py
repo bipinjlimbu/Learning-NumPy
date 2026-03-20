f4b93f2db9d7bff4568c1cb5c536b39d23d1e9a6
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-20 19:15:17 +0545

    Arithmetic Operations in NumPy

---
+++ 10.ArithmeticOperations.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+arr1 = np.array([1, 2, 3, 4, 5])
+arr2 = np.array([6, 7, 8, 9, 10])
+
+# Addition
+add_result = arr1 + arr2
+print("Addition:", add_result)
+
+# Subtraction
+sub_result = arr1 - arr2
+print("Subtraction:", sub_result)
+
+# Multiplication
+mul_result = arr1 * arr2
+print("Multiplication:", mul_result)
+
+# Division
+div_result = arr1 / arr2
+print("Division:", div_result)
+
+# Power
+pow_result = arr1 ** 2
+print("Power:", pow_result)
+
+# Modulo
+mod_result = arr1 % 2
+print("Modulo:", mod_result)
+
+# Floor Division
+floor_div_result = arr1 // 2
+print("Floor Division:", floor_div_result)
