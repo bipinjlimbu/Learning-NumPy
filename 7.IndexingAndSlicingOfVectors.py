5ec41eaad33583af120db60c7a9546ff4515dc08
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-20 19:05:02 +0545

    NumPy Indexing and Slicing of Vectors

--- 7.IndexingAndSlicingOfVectors.py
+++ 7.IndexingAndSlicingOfVectors.py
@@ -1,20 +1,15 @@
 import numpy as np
 
-arr = np.arange(1, 31).reshape(5, 6)
+arr = np.arange(1, 11)
 print("Original Array:")
 print(arr)
 
 # Indexing
-print("\nElement at row 2, column 3:", arr[1, 2]) # indexing starts at 0
-print("Element at row 4, column 5:", arr[3, 4])
+print("\nElement at index 0:", arr[0]) # indexing starts at 0
+print("Element at index 4:", arr[4])
 
 # Slicing
-print("\nSliced Array (rows 1 to 3, columns 2 to 4):")
-print(arr[0:3, 1:4]) # slicing is exclusive of the stop index
-print("\nSliced Array (all rows, columns 3 to 5):")
-print(arr[:, 2:5]) # slicing all rows, columns 2 to 4
-print("\nSliced Array (rows 2 to 5, all columns):")
-print(arr[1:5, :]) # slicing rows 1 to 4, all columns
-print("\nSliced Array (every other row and column):")
-print(arr[::2, ::2]) # slicing every other row and column
-
+print("\nSliced Array (index 2 to 5):")
+print(arr[2:6]) # slicing is exclusive of the stop index
+print("\nSliced Array (every other element):")
+print(arr[::2]) # slicing every other element
