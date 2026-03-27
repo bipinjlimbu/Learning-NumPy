5baa4c299c4fed7884cf83e649784c70297ecc69
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-27 20:41:32 +0545

    Question No 10 Completed

--- Exercises/QuestionNo10.py
+++ Exercises/QuestionNo10.py
@@ -2,9 +2,7 @@
 
 from QuestionData import data
 
-science = data[:, 2]
-for i in range(len(science)):
-    if science[i] < 75:
-        data[i][2] = 0
+science = data[:, 2] < 75
+data[science, 2] = 0
         
 print("Data after replacing Science scores less than 75 with 0:\n", data)
